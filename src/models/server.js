bec48a5fe2721a49b33afe2b97999190995a6f02
CHINOBv <retrochanel97@gmail.com>
2021-02-27 17:14:06 -0600

    CORS

--- src/models/server.js
+++ src/models/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
+const cors = require("cors");
 
 const Sockets = require("./sockets");
 
@@ -14,6 +15,7 @@ class Server {
     this.io = socketio(this.server);
   }
   middlewares() {
+    this.app.use(cors());
     this.app.use(express.static(path.resolve(__dirname, "../public")));
   }
 
